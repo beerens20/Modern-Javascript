114f99ff217f8ef88a8c3f4dbcc4d2d1636b4d54
Michael Beerens <beerens20@gmail.com>
2024-03-05 12:51:11 -0500

    added items to local storage
    set up functionality that adds items to local storage for retention

--- 09 Shopping List Project/script.js
+++ 09 Shopping List Project/script.js
@@ -19,7 +19,7 @@ const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 
 /* Create eventListener functions */
-function addItem(e){
+function onAddItemSubmit(e){
     // prevent form submission to submitting to a file
     e.preventDefault();
 
@@ -31,10 +31,24 @@ function addItem(e){
         alert('Please add an item');
         return;
     }
+    
+    //Create item DOM element
+    addItemtoDOM(newItem);
+
+    //Add item to local storage
+    addItemToStorage(newItem);
+
+    checkUI();
+    itemInput.value = '';
+}
+
+
+
+function addItemtoDOM(item){
     // create list item
     const li = document.createElement('li');
     // put value from newItem into the li element
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(item));
     console.log(li);
 
     // call createButton function to create our button with the necessary classes
@@ -43,9 +57,26 @@ function addItem(e){
 
     // Add li to the DOM
     itemList.appendChild(li);
-    checkUI();
-    itemInput.value = '';
-}
+};
+
+function addItemToStorage(item){
+    //Create a placeholder variable
+    let itemsFromStorage;
+
+    //Check to see if there are already items in localStorage
+    if (localStorage.getItem('items') === null){
+        //If localStorage is empty set itemsFromStorage to an empty array
+        itemsFromStorage = [];
+    }else {
+        //If localStorage is NOT empty parse localStorage back into an array
+        //and put new item into the array
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+    };
+    itemsFromStorage.push(item);
+
+    //Convert updated array to a JSON string and set it back to localStorage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+};
 
 function createButton(classes){
     const button = document.createElement('button');
@@ -80,6 +111,27 @@ function clearItems(){
     checkUI();
 };
 
+function filterItems(e){
+    // captures all li's in itemList (creates node list (array))
+    const items = itemList.querySelectorAll('li');
+    //capture text being typed
+    const text = e.target.value.toLowerCase();
+
+    //Loop through items node list (which is an array)
+    items.forEach(item => {
+        //Return the first child of the item element which is the text node
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        //Match the filter input to the list items
+        if (itemName.indexOf(text) != -1){
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+    
+};
+
 // Clear UI state (hide filter and clear button when list is empty)
 function checkUI(){
     // captures all li's in itemList (creates node list (array))
@@ -96,13 +148,16 @@ function checkUI(){
 
 /* Create event listeners */
 // form submit button
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 
 // delete item event listener (target UL for event delegation)
 itemList.addEventListener('click', removeItem);
 
 // clear all items
 clearBtn.addEventListener('click', clearItems);
+
+// filter list items
+itemFilter.addEventListener('input', filterItems)
 
 // check UI when page loads
 checkUI();
